3a4077693cdb782b9d0a3af478d593ff88518ae5
TRI AFRILIYANTI <71365606+triafri@users.noreply.github.com>
2020-09-28 14:28:23 +0700

    Add files via upload

---
+++ src/Page/Footer.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Topevents from '../Events/TopEvents';
+
+class Footer extends Component{
+    render(){
+        return(
+            <div>
+                <Topevents/>
+                <h3> Tugas 3 React Js</h3>
+            </div>
+        )
+    }
+}
+
+export default Footer
---
+++ src/Page/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(){
+	return(
+		<div>
+			<h2 align="center"> Home  |  Product  |  Kontak  |  Tentang Kami </h2>
+		</div>
+		)
+}
+
+export default Header;
